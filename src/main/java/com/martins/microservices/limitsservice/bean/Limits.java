b6c4cd601c1cd953982d0f0a83919bdf044dc032
Lucas Martins <lucasrodriguesmartinsoliveira@gmail.com>
2024-02-19 20:42:12 -0300

    implementing the initials configurations and endpoints

---
+++ src/main/java/com/martins/microservices/limitsservice/bean/Limits.java
@@ -0,0 +1,33 @@
+package com.martins.microservices.limitsservice.bean;
+
+public class Limits {
+
+    private int minimun;
+
+    private int maximun;
+
+
+    public Limits() {
+    }
+
+    public Limits(int minimun, int maximun) {
+        this.minimun = minimun;
+        this.maximun = maximun;
+    }
+
+    public int getMinimun() {
+        return minimun;
+    }
+
+    public void setMinimun(int minimun) {
+        this.minimun = minimun;
+    }
+
+    public int getMaximun() {
+        return maximun;
+    }
+
+    public void setMaximun(int maximun) {
+        this.maximun = maximun;
+    }
+}
